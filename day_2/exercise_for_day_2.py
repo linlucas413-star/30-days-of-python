cc77a677cd02ea2ff1163048a6e9c5e527f19ccc
linlucas413-star <linlucas413@gmail.com>
2026-06-16 18:38:56 -0500

    second day of learning code from scratch

---
+++ day_2/exercise_for_day_2.py
@@ -0,0 +1,31 @@
+import math
+#Day Two lets go!
+#Exercise level 1
+first_name = "lucas"
+last_name = "lin"
+full_name = "lucas lin"
+country = "USA"
+city = "Dallas"
+age = 18
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+print(f"my name is {first_name} and i am {age} and from {city}")
+
+#Exercise level 2 lets go
+print(type(first_name),type(age),type(is_married))
+print(len(first_name))
+num_one = 5
+num_two = 4
+total = (num_one + num_two)
+print(total)
+difference = (num_one - num_two)
+print(difference)
+# finding radius of circle exercise
+radius = input("What is the radius? ")
+
+area_of_circle = (math.pi * (float(radius, 2) ** 2))
+print(f"the radius of your circle is: {area_of_circle}")
+
+#okay thats good light work no reaction
