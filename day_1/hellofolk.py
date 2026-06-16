dc110cf69f5f581b0bd967fed6acedf505a5f528
linlucas413-star <linlucas413@gmail.com>
2026-06-16 18:02:30 -0500

    printing a bunch of stuff

---
+++ day_1/hellofolk.py
@@ -0,0 +1,19 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4)
+print(3 ** 4)
+print(3 // 4)
+print("Hi everyone my name is Lucas Lin")
+print(type(10))
+print(type(6.7))
+print(type(4-4j))
+print(type({"name":"lucaslin"}))
+
+# Okay i have no clue whats going on but im gonna try the level 3 exercise
+#finding euclidean distance between (2,3) and (10, 8)
+#its something like a^2 plus b^2 equals c^squared right? 
+print(((8 ** 2) + (5 ** 2)) ** (1/2))
+# console says 9.433 something ... imma check if thats right
+#yay!
