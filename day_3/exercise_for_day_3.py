68c37d032b27d17d60c9a2f603b1513d414b83ca
linlucas413-star <linlucas413@gmail.com>
2026-06-19 14:01:41 -0500

    day 3

---
+++ day_3/exercise_for_day_3.py
@@ -0,0 +1,32 @@
+#Exercise for day 3
+
+#Area of Triangle
+#Base = float(input("what is the base?:" ))
+#Height = float(input("What is the height:"))
+#area_of_triangle = (Base * Height * (.5))
+#print("area of triangle:", area_of_triangle)
+
+#Permimeter of Traingle
+#a = float(input("How long is side a?: "))
+#b = float(input('How long is side b?: '))
+#c = float(input('How long is side c?: '))
+#perimeter = (a + b + c)
+#print('the perimeter of your tirangle is: ', perimeter, 'units')
+
+#print("'Jargon' in: I hope this course isn't full of jargon:", 'jargon' in "I hope this course isn't full of jargon")
+
+#floor_div = (7 // 3)
+#int_value = int(2.3)
+#print(int_value)
+#print(floor_div)
+#print("floor division of 7 and 3 equal to integer value of 2.3?: ", floor_div is int_value)
+
+#type_1 = type('10')
+#type_2 = type(10)
+#print("type_1 = type_2: ", type_1 == type_2)
+
+#payment calculartor
+#hourly_pay = float(input("How much do you get paid by the hour? "))
+#hours_worked = float(input("How many hours do you work per week? "))
+#weekly_pay = (hourly_pay * hours_worked)
+#print("Your weekly pay is:", weekly_pay,'$')
